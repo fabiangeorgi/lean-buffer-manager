7877fef36e4e2d61d28e6f615d66fead9f5b233f
Fabian Georgi <fabian.georgi@student.hpi.uni-potsdam.de>
2023-07-01 12:31:19 +0200

    implemented ssd region

--- src/data_regions.cpp
+++ src/data_regions.cpp
@@ -17,7 +17,7 @@ VolatileRegion::VolatileRegion(uint64_t frame_count) : _frame_count{frame_count}
     // -- TODO(student) get a memory region for volatile buffer frames.
     // TODO check flags
     _data = reinterpret_cast<std::byte *>(mmap(nullptr, frame_count * sizeof(BufferFrame), PROT_READ | PROT_WRITE,
-                                          MAP_PRIVATE | MAP_ANONYMOUS, -1, 0));
+                                               MAP_PRIVATE | MAP_ANONYMOUS, -1, 0));
     madvise(_data, frame_count * sizeof(BufferFrame), MADV_HUGEPAGE);
     // TODO(student) uncomment `init_free_frames` call in line below once data is allocated.
     _init_free_frames();
@@ -42,8 +42,8 @@ BufferFrame *VolatileRegion::allocate_frame() {
 void VolatileRegion::free_frame(BufferFrame *frame) {
     // -- TODO(student) manage the region's state so that this frame is indicated as free now. Thus, it can be used and
     // returned in `allocate_frame`.
-    auto* test = new (frame) BufferFrame();
-    _free_frames[free_frame_count()] = test;
+    // TODO append at back
+    _free_frames[free_frame_count()] = new(frame) BufferFrame();
     _free_frame_count++;
 }
 
@@ -82,45 +82,65 @@ void VolatileRegion::_init_free_frames() {
 ///////////////////////////////////////////////////////////
 
 // TODO(student) implement
-SSDRegion::SSDRegion(const std::filesystem::path &file_path, uint64_t page_count) {
+SSDRegion::SSDRegion(const std::filesystem::path &file_path, uint64_t page_count) : _page_count(page_count) {
     // open the file at `file_path`. Note, we want to read and write data. If the file already exists, overwrite it. Note,
     // that O_DIRECT is required.
+    // open file
+    // O_CREAT -> create if not exist
+    // O_TRUNC -> TRUNCATE to 0 if exists
+    // O_RDWR -> READ and WRITE
+    // O_DIRECT -> direct disk access -> kernel does not get involved
+    _file = open(file_path.c_str(), O_CREAT | O_RDWR | O_DIRECT | O_TRUNC);
+    // should contain page_count pages
+    auto dummy_data = (uint8_t *) aligned_alloc(sizeof(Page), page_count * sizeof(Page));
+    pwrite(_file, dummy_data, page_count * sizeof(Page), 0);
+
+    free(dummy_data);
     _init_free_pages();
 }
 
 uint64_t SSDRegion::page_count() const {
     // TODO(student) implement
-    return 0;
+    return _page_count;
 }
 
 uint64_t SSDRegion::free_page_count() const {
     // TODO(student) implement
-    return 0;
+    return _free_pages.size();
 }
 
 PageID SSDRegion::allocate_page_id() {
-    // TODO(student) implement
-    return 0;
+    PageID freePageId = _free_pages.front();
+    _free_pages.pop_front();
+    return freePageId;
 }
 
 void SSDRegion::free_page_id(PageID page_id) {
     // TODO(student) implement
+    _free_pages.push_front(page_id);
 }
 
 SSDRegion::~SSDRegion() {
     // TODO(student) free resources
+    close(_file);
 }
 
 void SSDRegion::read_page(std::byte *destination, PageID page_id) {
     // TODO(student) read data from page at `page_id` into `destination`.
+    pread(_file, destination, sizeof(Page), page_id * sizeof(Page));
 }
 
 void SSDRegion::write_page(const std::byte *source, PageID page_id) {
     // TODO(student) write data from page at `source` into the page with `page_id` on disk. You need to explicitly flush
     // data when writing to disk, as this is otherwise not guaranteed to be persistent in case of power loss.
+    pwrite(_file, source, sizeof(Page), page_id * sizeof(Page));
+    fsync(_file);
 }
 
 void SSDRegion::_init_free_pages() {
     // -- TODO(student) setup required variables that you might use to fulfill the requirement of `allocate_page_id`,
     // i.e., page IDs are ascending, starting with 0, but a most recently freed page ID is allocated first.
+    for (PageID i = 0; i < _page_count; i++) {
+        _free_pages.push_back(i);
+    }
 }
--- src/data_regions.hpp
+++ src/data_regions.hpp
@@ -10,95 +10,106 @@
 class BufferManager;
 
 class VolatileRegion {
- public:
-  // Allocates memory for volatile BufferFrames, i.e., this memory range holds all data that you will read and modify
-  // while in memory. This is the in-memory part of the buffer pool. Note: The memory allocated here must be aligned
-  // for BufferFrames, i.e., we must be able to create a BufferFrame at the 0th byte of the data. With the current
-  // implementation of the utility functions data_begin() and data_end() (see below), the member variables _data needs
-  // to store the start address of the allocated memory region. If you do not store the start address pointer in _data,
-  // you need to modify data_begin() and data_end() accordingly.
-  explicit VolatileRegion(uint64_t frame_count);
+public:
+    // Allocates memory for volatile BufferFrames, i.e., this memory range holds all data that you will read and modify
+    // while in memory. This is the in-memory part of the buffer pool. Note: The memory allocated here must be aligned
+    // for BufferFrames, i.e., we must be able to create a BufferFrame at the 0th byte of the data. With the current
+    // implementation of the utility functions data_begin() and data_end() (see below), the member variables _data needs
+    // to store the start address of the allocated memory region. If you do not store the start address pointer in _data,
+    // you need to modify data_begin() and data_end() accordingly.
+    explicit VolatileRegion(uint64_t frame_count);
 
-  // Free all acquired resources.
-  ~VolatileRegion();
+    // Free all acquired resources.
+    ~VolatileRegion();
 
-  // Create a new frame within the volatile region to use. The function assumes that at least one free frame is
-  // available. Thus, the caller has to make sure that at least one frame is free.
-  BufferFrame* allocate_frame();
+    // Create a new frame within the volatile region to use. The function assumes that at least one free frame is
+    // available. Thus, the caller has to make sure that at least one frame is free.
+    BufferFrame *allocate_frame();
 
-  // Frees the memory of the volatile `frame`. Thus, the frame's memory region can be reused for other frame allocations
-  // afterwards. Note that modified pages stored in a buffer frame should be flushed before calling this function.
-  // Otherwise, the data changes are lost.
-  void free_frame(BufferFrame* frame);
+    // Frees the memory of the volatile `frame`. Thus, the frame's memory region can be reused for other frame allocations
+    // afterwards. Note that modified pages stored in a buffer frame should be flushed before calling this function.
+    // Otherwise, the data changes are lost.
+    void free_frame(BufferFrame *frame);
 
-  // Returns the pointer to the volatile data/memory region as a BufferFrame*. This allows accessing all
-  // of the volatile region's frames.
-  BufferFrame* frames();
+    // Returns the pointer to the volatile data/memory region as a BufferFrame*. This allows accessing all
+    // of the volatile region's frames.
+    BufferFrame *frames();
 
-  // Returns the total number of volatile data region's frames.
-  uint64_t frame_count() const;
+    // Returns the total number of volatile data region's frames.
+    uint64_t frame_count() const;
 
-  // Returns the number of free frames in the volatile data region.
-  uint64_t free_frame_count() const;
+    // Returns the number of free frames in the volatile data region.
+    uint64_t free_frame_count() const;
 
-  // Helper methods for tests. Do not modify!
-  bool address_in_range(const void* addr) const { return data_begin() <= addr && addr < data_end(); }
-  std::byte* data_begin() const { return _data; }
-  std::byte* data_end() const { return _data + _frame_count * sizeof(BufferFrame); }
+    // Helper methods for tests. Do not modify!
+    bool address_in_range(const void *addr) const { return data_begin() <= addr && addr < data_end(); }
 
-  // Delete move and copy
-  VolatileRegion(const VolatileRegion&) = delete;
-  VolatileRegion(VolatileRegion&&) = delete;
-  VolatileRegion& operator=(const VolatileRegion&) = delete;
-  VolatileRegion& operator=(VolatileRegion&&) = delete;
+    std::byte *data_begin() const { return _data; }
 
- private:
-  void _init_free_frames();
+    std::byte *data_end() const { return _data + _frame_count * sizeof(BufferFrame); }
 
-  std::byte* _data = nullptr;
-  const uint64_t _frame_count;
-  uint64_t _free_frame_count;
-  std::vector<BufferFrame*> _free_frames{};
+    // Delete move and copy
+    VolatileRegion(const VolatileRegion &) = delete;
+
+    VolatileRegion(VolatileRegion &&) = delete;
+
+    VolatileRegion &operator=(const VolatileRegion &) = delete;
+
+    VolatileRegion &operator=(VolatileRegion &&) = delete;
+
+private:
+    void _init_free_frames();
+
+    std::byte *_data = nullptr;
+    const uint64_t _frame_count;
+    uint64_t _free_frame_count;
+    std::vector<BufferFrame *> _free_frames{};
 };
 
 // This class represents the data on disk. Pages are logically mapped from 0 to n via their page id. So page 0 starts
 // at offset 0, page 1 starts at 1 * PAGE_SIZE, page 2 starts at 2 * PAGE_SIZE, and page k starts at k * PAGE_SIZE.
 class SSDRegion {
- public:
-  // Opens the file at `file_path`. Overwrite an existing file and resize it to contain `page_count` pages.
-  SSDRegion(const std::filesystem::path& file_path, uint64_t page_count);
+public:
+    // Opens the file at `file_path`. Overwrite an existing file and resize it to contain `page_count` pages.
+    SSDRegion(const std::filesystem::path &file_path, uint64_t page_count);
 
-  // Free all acquired resources.
-  ~SSDRegion();
+    // Free all acquired resources.
+    ~SSDRegion();
 
-  // Returns the next available page ID. Page IDs are ascending starting with 0. However, page IDs can be freed, when
-  // they are not required anymore. If page IDs get freed, this function returns the most recently freed page id.
-  PageID allocate_page_id();
+    // Returns the next available page ID. Page IDs are ascending starting with 0. However, page IDs can be freed, when
+    // they are not required anymore. If page IDs get freed, this function returns the most recently freed page id.
+    PageID allocate_page_id();
 
-  // Frees the given page_id so that it is available for allocation (see `allocate_page_id`).
-  void free_page_id(PageID page_id);
+    // Frees the given page_id so that it is available for allocation (see `allocate_page_id`).
+    void free_page_id(PageID page_id);
 
-  // Reads an entire page (= PAGE_SIZE) with `page_id` from the backing file into `destination`.
-  void read_page(std::byte* destination, PageID page_id);
+    // Reads an entire page (= PAGE_SIZE) with `page_id` from the backing file into `destination`.
+    void read_page(std::byte *destination, PageID page_id);
 
-  // Writes an entire page (= PAGE_SIZE) with `page_id` from `source` to the backing file.
-  void write_page(const std::byte* source, PageID page_id);
+    // Writes an entire page (= PAGE_SIZE) with `page_id` from `source` to the backing file.
+    void write_page(const std::byte *source, PageID page_id);
 
-  // Returns the total number of the SSD data region's pages (including unwritten ones).
-  uint64_t page_count() const;
+    // Returns the total number of the SSD data region's pages (including unwritten ones).
+    uint64_t page_count() const;
 
-  // Returns the number of available, i.e., currently not allocated, pages in the SSD data region.
-  uint64_t free_page_count() const;
+    // Returns the number of available, i.e., currently not allocated, pages in the SSD data region.
+    uint64_t free_page_count() const;
 
-  // Delete move and copy
-  SSDRegion(const SSDRegion&) = delete;
-  SSDRegion(SSDRegion&&) = delete;
-  SSDRegion& operator=(const SSDRegion&) = delete;
-  SSDRegion& operator=(SSDRegion&&) = delete;
+    // Delete move and copy
+    SSDRegion(const SSDRegion &) = delete;
 
- private:
-  void _init_free_pages();
+    SSDRegion(SSDRegion &&) = delete;
 
-  // TODO(student) Feel free to add further variables or functions if needed.
-  // ...
+    SSDRegion &operator=(const SSDRegion &) = delete;
+
+    SSDRegion &operator=(SSDRegion &&) = delete;
+
+private:
+    void _init_free_pages();
+
+    // TODO(student) Feel free to add further variables or functions if needed.
+    // ...
+    int32_t _file;
+    uint64_t _page_count;
+    std::deque<PageID> _free_pages{};
 };
